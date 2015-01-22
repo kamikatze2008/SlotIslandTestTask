a2e0e5c0ac81ba6559e05e2d210b5184ad50212c
Serhii Denysov <sergeydenisovcad@gmail.com>
2015-01-22 19:57:56 +0200

    Third commit

--- src/com/slotisland/denysov/comparator/KeyValueComparator.java
+++ src/com/slotisland/denysov/comparator/KeyValueComparator.java
@@ -76,12 +76,12 @@ public class KeyValueComparator implements LineComparator {
 
     private String compareValues() {
         Iterator<String> keySetIterator = keySet.iterator();
-        StringBuilder resultString=new StringBuilder();
+        StringBuilder resultString = new StringBuilder();
         String tempKey;
         Pattern numberPattern = Pattern.compile("(\\d)+(\\.)*(\\d)*");
         Pattern arrayPattern = Pattern.compile("((\\d)+(\\.)*(\\d)*,)+(\\d)+(\\.)*(\\d)*");
         Matcher firstNumberMatcher, secondNumberMatcher;
-        Matcher firstArrayMatcher,secondArrayMatcher;
+        Matcher firstArrayMatcher, secondArrayMatcher;
         String firstStringValue;
         String secondStringValue;
         while (keySetIterator.hasNext()) {
@@ -89,40 +89,48 @@ public class KeyValueComparator implements LineComparator {
             firstStringValue = firstHashMap.get(tempKey);
             secondStringValue = secondHashMap.get(tempKey);
             if (!firstStringValue.equals(secondStringValue)) {
-                firstNumberMatcher=numberPattern.matcher(firstStringValue);
-                secondNumberMatcher=numberPattern.matcher(secondStringValue);
-                firstArrayMatcher=arrayPattern.matcher(firstStringValue);
-                secondArrayMatcher=arrayPattern.matcher(secondStringValue);
-                if(firstNumberMatcher.matches() && secondNumberMatcher.matches()){
-                    double firstValue=Double.parseDouble(firstStringValue);
-                    double secondValue=Double.parseDouble(secondStringValue);
-                    if(Math.abs(firstValue-secondValue)>0.001){
-                        resultString.append("Line ").append(lineNumber).append(". Different numbers in \"").
-                                append(tempKey).append("\" tag.\n");
-                    }
-                } else if(firstArrayMatcher.matches() && secondArrayMatcher.matches()){
-                    StringTokenizer firstStringTokenizer = new StringTokenizer(firstStringValue,",");
-                    StringTokenizer secondStringTokenizer = new StringTokenizer(secondStringValue,",");
-                    List<Double> firstArrayList = new ArrayList<Double>();
-                    List<Double> secondArrayList= new ArrayList<Double>();
-                    while (firstStringTokenizer.hasMoreTokens()){
-                        firstArrayList.add(Double.parseDouble(firstStringTokenizer.nextToken()));
-                    }
-                    while (secondStringTokenizer.hasMoreTokens()){
-                        secondArrayList.add(Double.parseDouble(secondStringTokenizer.nextToken()));
-                    }
-                    Collections.sort(firstArrayList);
-                    Collections.sort(secondArrayList);
-                    if(!firstArrayList.equals(secondArrayList)){
-                        resultString.append("Line ").append(lineNumber).append(". Different arrays in \"").
-                                append(tempKey).append("\" tag.\n");
-                    }
-                } else{
+                firstNumberMatcher = numberPattern.matcher(firstStringValue);
+                secondNumberMatcher = numberPattern.matcher(secondStringValue);
+                firstArrayMatcher = arrayPattern.matcher(firstStringValue);
+                secondArrayMatcher = arrayPattern.matcher(secondStringValue);
+                if (firstNumberMatcher.matches() && secondNumberMatcher.matches()) {
+                    numberCheck(resultString, tempKey, firstStringValue, secondStringValue);
+                } else if (firstArrayMatcher.matches() && secondArrayMatcher.matches()) {
+                    arrayCheck(resultString, tempKey, firstStringValue, secondStringValue);
+                } else {
                     resultString.append("Line ").append(lineNumber).append(". Different value types in \"").
                             append(tempKey).append("\" tag.\n");
                 }
             }
         }
         return resultString.toString();
+    }
+
+    private void arrayCheck(StringBuilder resultString, String tempKey, String firstStringValue, String secondStringValue) {
+        StringTokenizer firstStringTokenizer = new StringTokenizer(firstStringValue, ",");
+        StringTokenizer secondStringTokenizer = new StringTokenizer(secondStringValue, ",");
+        List<Double> firstArrayList = new ArrayList<Double>();
+        List<Double> secondArrayList = new ArrayList<Double>();
+        while (firstStringTokenizer.hasMoreTokens()) {
+            firstArrayList.add(Double.parseDouble(firstStringTokenizer.nextToken()));
+        }
+        while (secondStringTokenizer.hasMoreTokens()) {
+            secondArrayList.add(Double.parseDouble(secondStringTokenizer.nextToken()));
+        }
+        Collections.sort(firstArrayList);
+        Collections.sort(secondArrayList);
+        if (!firstArrayList.equals(secondArrayList)) {
+            resultString.append("Line ").append(lineNumber).append(". Different arrays in \"").
+                    append(tempKey).append("\" tag.\n");
+        }
+    }
+
+    private void numberCheck(StringBuilder resultString, String tempKey, String firstStringValue, String secondStringValue) {
+        double firstValue = Double.parseDouble(firstStringValue);
+        double secondValue = Double.parseDouble(secondStringValue);
+        if (Math.abs(firstValue - secondValue) > 0.001) {
+            resultString.append("Line ").append(lineNumber).append(". Different numbers in \"").
+                    append(tempKey).append("\" tag.\n");
+        }
     }
 }
